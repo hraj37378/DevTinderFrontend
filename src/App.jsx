098471ab186931e9d45a5b255db102e187c23293
hraj37378 <hraj37378@gmail.com>
2025-01-11 19:58:25 +0530

    add initial components and setup for routing with Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import Body from "./Body";
+
 function App() {
   return(
     <>
-    <h1>Hello World</h1>
+    <BrowserRouter basename="/">
+    <Routes>
+    <Route path = "/" element ={<Body/>}>
+      <Route path = "/login" element = {<Login/>}/>
+      <Route path = "/test" element ={<div>Test Page</div>}/>
+    </Route>
+    </Routes>
+    </BrowserRouter>  
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer footer-center bg-base-200 text-base-content p-4 fixed bottom-0">
+      <aside>
+        <p>
+          Copyright © {new Date().getFullYear()} - All right reserved by DevTinder 
+        </p>
+      </aside>
+    </footer>
+  );
+};
+
+export default Footer;
