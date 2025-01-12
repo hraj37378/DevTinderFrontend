61a06a0ea7f2b1e9cdf660210192f56a6e1dcaea
hraj37378 <hraj37378@gmail.com>
2025-01-12 19:33:29 +0530

    feat: implement feed functionality with Redux and add UserCard component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import axios from "axios";
+import { useEffect } from "react";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import UserCard from "./UserCard";
 
 const Feed = () => {
+  const feedData = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+  const getFeed = async () => {
+    if (feedData) return;
+    try {
+      const res = await axios.get(BASE_URL + "/user/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res?.data?.data));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  useEffect(() => {
+    getFeed();
+  }, []);
+
   return (
-    <div>Feed</div>
+    feedData && (
+      <div className="flex justify-center my-10">
+       <UserCard user = {feedData[0]}/>
+      </div>
+    )
   )
-}
+};
 
 export default Feed;
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,28 @@
+const UserCard = ({ user }) => {
+    console.log("user ", user );
+   const {firstName, lastName, age, imageUrl, about} = user;
+
+  return (
+    <div>
+      <div className="card card-side bg-base-300 shadow-xl">
+        <figure>
+          <img
+            src= {imageUrl}
+            alt="image"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{ firstName + " " + lastName}</h2>
+          {age && gender && <p>{age + ", " + gender}</p>}
+          <p>{about}</p>
+          <div className="card-actions justify-center my-4">
+            <button className="btn btn-secondary">Like</button>
+            <button className="btn btn-primary">Dislike</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
